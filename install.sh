da4f8cf90393d9775e67542fc28bf1269ee51e23
cl0ten <163528456+cl0one@users.noreply.github.com>
2024-03-17 12:46:53 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+. /etc/os-release
+
+if ! command -v sudo &>/dev/null; then
+    echo "Error: sudo command not found. Please make sure that you run the installation command with sudo bin/bash .."
+    exit 1
+fi
+
+sudo wget -qO- https://deb.dmz.ator.dev/anon.asc | sudo tee /etc/apt/trusted.gpg.d/anon.asc
+
+sudo echo "deb [signed-by=/etc/apt/trusted.gpg.d/anon.asc] https://deb.dmz.ator.dev anon-stage-$VERSION_CODENAME main" | sudo tee /etc/apt/sources.list.d/anon.list
+
+sudo apt-get update --yes && sudo apt-get install anon --yes
+
+sudo mv /etc/anon/anonrc /etc/anon/anonrc.bak
+
+read -p "Enter your desired nickname for the Anon Relay (1-19 characters, only [a-zA-Z0-9] and no spaces): " NICKNAME
+while ! [[ "$NICKNAME" =~ ^[a-zA-Z0-9]{1,19}$ ]]; do
+    echo "Error: Invalid nickname format. Please enter 1-19 characters, only [a-zA-Z0-9] and no spaces."
+    read -p "Enter your desired nickname for the Anon Relay (1-19 characters, only [a-zA-Z0-9] and no spaces): " NICKNAME
+done
+
+read -p "Enter your contact information for the Anon Relay: " CONTACT_INFO
+
+read -p "Enter comma-separated fingerprints for your relay's family (leave empty to skip): " MY_FAMILY
+
+while [[ -n "$MY_FAMILY" && ! "$MY_FAMILY" =~ ^([A-Z0-9]+,)*[A-Z0-9]+$ ]]; do
+    echo "Error: Invalid MyFamily format. Please enter comma-separated fingerprints with only capital letters."
+    read -p "Enter comma-separated fingerprints for your relay's family (leave empty to skip): " MY_FAMILY
+done
+
+read -p "Enter BandwidthRate in Mbit (leave empty to skip): " BANDWIDTH_RATE
+read -p "Enter BandwidthBurst in Mbit (leave empty to skip): " BANDWIDTH_BURST
+
+cat <<EOF | sudo tee /etc/anon/anonrc >/dev/null
+Log notice file /var/log/anon/notices.log
+ORPort 9001
+ControlPort 9051
+SocksPort 0
+ExitRelay 0
+IPv6Exit 0
+ExitPolicy reject *:*
+ExitPolicy reject6 *:*
+
+$( [[ -n "$BANDWIDTH_RATE" ]] && echo "BandwidthRate $BANDWIDTH_RATE" )
+$( [[ -n "$BANDWIDTH_BURST" ]] && echo "BandwidthBurst $BANDWIDTH_BURST" )
+Nickname $NICKNAME
+Contactinfo $CONTACT_INFO
+EOF
+
+if [[ -n "$MY_FAMILY" ]]; then
+    echo "MyFamily $MY_FAMILY" | sudo tee -a /etc/anon/anonrc >/dev/null
+fi
+
+sudo systemctl restart anon.service
+
+echo "Anon installation completed successfully."
