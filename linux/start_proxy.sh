5c53c576e5b0f924f418adba78eee4929c314ec2
rA3ka <160638097+rA3ka@users.noreply.github.com>
2024-09-18 01:39:18 +0200

    Update start_proxy.sh

--- linux/start_proxy.sh
+++ linux/start_proxy.sh
@@ -29,7 +29,7 @@ if [[ "$(uname)" == "Linux" ]];then
 
 	ExitIP="$(curl -s --socks5 127.0.0.1:9050 https://check.en.anyone.tech/api/ip -s | jq .IP | cut -d'"' -f2)"
 	IsAnon="$(curl -s --socks5 127.0.0.1:9050 https://check.en.anyone.tech/api/ip -s | jq .IsAnon)"
-	ExitCountry="$(whois $ExitIP | grep -m1 country | awk '{print $2}')"
+	ExitCountry="$(curl --socks4 127.0.0.1:9050 -s https://ipinfo.io | grep country | cut -d'"' -f4)"
 	echo -e "\nExit IP: $ExitIP\nExit Country: $ExitCountry\nIs Anon: $IsAnon\n"
 
 	function handle_sigint() {
