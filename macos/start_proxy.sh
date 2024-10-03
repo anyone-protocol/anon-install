cdf3baace6b1de1cc206ca39425090f4af054e12
rA3ka <160638097+rA3ka@users.noreply.github.com>
2024-10-03 02:46:06 +0200

    Update start_proxy.sh

--- macos/start_proxy.sh
+++ macos/start_proxy.sh
@@ -4,8 +4,18 @@ if [[ "$(sw_vers --productName)" == "macOS" ]];then
 
 
     if [[ ! -f anon-live-macos-$(uname -p)64.zip ]];then
-      curl -o anon.zip -fsSLO https://github.com/anyone-protocol/ator-protocol/releases/download/v0.4.9.6/anon-live-macos-$(uname -p)64.zip
+      curl -m 5 -o anon.zip -fsSLO https://github.com/anyone-protocol/ator-protocol/releases/download/v0.4.9.6/anon-live-macos-$(uname -p)64.zip
     fi
+
+    function handle_sigint() {
+      networksetup -setsocksfirewallproxystate "Wi-Fi" off
+      rm anon.zip
+      rm anon
+      rm anonrc
+      exit 0
+    }
+    
+    trap handle_sigint INT
     
     unzip anon.zip anon > /dev/null 2>&1
     
@@ -18,35 +28,19 @@ if [[ "$(sw_vers --productName)" == "macOS" ]];then
     sleep 1
     
     networksetup -setsocksfirewallproxy "Wi-Fi" 127.0.0.1 9050
-    networksetup -setsecurewebproxy "Wi-Fi" 127.0.0.1 9058
-    networksetup -setwebproxy "Wi-Fi" 127.0.0.1 9058
-    
     networksetup -setsocksfirewallproxystate "Wi-Fi" on
-    networksetup -setsecurewebproxystate "Wi-Fi" on
-    networksetup -setwebproxystate "Wi-Fi" on
     
     sleep 1
     
-	ExitIP="$(curl -s --socks5 127.0.0.1:9050 https://check.en.anyone.tech/api/ip -s | cut -d'"' -f6)"
-	IsAnon="$(curl -s --socks5 127.0.0.1:9050 https://check.en.anyone.tech/api/ip -s | cut -d':' -f2 | cut -d',' -f1)"
+    CheckAnon=$(curl -s --socks5 127.0.0.1:9050 https://check.en.anyone.tech/api/ip)
+	ExitIP="$(echo $CheckAnon | cut -d'"' -f6)"
+	IsAnon="$(echo $CheckAnon | cut -d':' -f2 | cut -d',' -f1)"
 	ExitCountry="$(curl --socks4 127.0.0.1:9050 -s https://ipinfo.io | grep country | cut -d'"' -f4)"
-    echo -e "\nExit IP: $ExitIP\nExit Country: $ExitCountry\nIs Anon: $IsAnon\n"
-    
-    function handle_sigint() {
-      networksetup -setsocksfirewallproxystate "Wi-Fi" off
-      networksetup -setsecurewebproxystate "Wi-Fi" off
-      networksetup -setwebproxystate "Wi-Fi" off
-      rm anon.zip
-      rm anon
-      rm anonrc
-      exit 0
-    }
-    
-    trap handle_sigint INT
+    echo -e "\nExit IP: $ExitIP\nExit Country: $ExitCountry\nIs Anon: $IsAnon"
     
     while true; do
-      echo "Press Cmd+C to quit proxy"
-      sleep 600
+      echo -e "\nPress Cmd+C to quit proxy"
+      sleep 120
     done
 
 else
